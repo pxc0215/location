1d758474401479970746711eb9a37d88def35f8c
panxc <panxc@fenbi.com>
2024-12-18 15:09:32 +0800

    调整订单列表样式

--- miniprogram/pages/orderlist/orderlist.js
+++ miniprogram/pages/orderlist/orderlist.js
@@ -69,6 +69,10 @@ Page({
   },
 
   getOrderList() {
+    wx.showLoading({
+      title: '加载中...'
+    })
+
     var env = require('../../envList.js').dev
     db.collection(app.globalData.collection_order + '_' + env).where(db.command.or([{
       _openid: this.data.openId
@@ -77,8 +81,31 @@ Page({
     }])).get({
       success: function (res) {
         console.log("res = ", res.data)
+        // 处理每个订单的时间显示
+        const orders = res.data.map(order => {
+          // 如果存在 createTime 字段且为时间戳
+          if (order.createTime) {
+            const date = new Date(order.createTime);
+            order.createTime = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
+          } else {
+            // 如果没有 createTime，使用数据库自带的 _createTime
+            const date = new Date(order._createTime || Date.now());
+            order.createTime = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
+          }
+          return order;
+        });
+
         that.setData({
-          orders: res.data
+          orders: orders
+        })
+        wx.hideLoading()
+      },
+      fail: function(err) {
+        console.error('获取订单列表失败：', err)
+        wx.hideLoading()
+        wx.showToast({
+          title: '加载失败',
+          icon: 'error'
         })
       }
     })
